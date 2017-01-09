287686aae65c73d5c40cee6a8b929d2f99b382bb
mattkcole <mattcole1093@gmail.com>
2017-01-08 23:43:04 -0500

    finishing nice_table funct

--- R/hello.R
+++ R/hello.R
@@ -1,8 +1,3 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
 # You can learn more about package authoring with RStudio at:
 #
 #   http://r-pkgs.had.co.nz/
@@ -12,6 +7,7 @@
 #   Build and Reload Package:  'Cmd + Shift + B'
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
+library(dplyr)
 
 df_rename <- function(data_set, names, title) {
 
@@ -23,4 +19,28 @@ df_rename <- function(data_set, names, title) {
 
 }
 
+library(dplyr)
+library(broom)
+library(knitr)
+
+nice_table <- function(fit, level = 0.95, roundto = 2){
+        low_ <- paste(signif(level / 2 * 100,2))
+        high_ <- paste(signif((1-level) * 100,2))
+
+        fit_frame <- fit %>%
+                tidy() %>%
+                cbind(confint(fit, level = level)) %>%
+                dplyr::select(estimate, `2.5 %`, `97.5 %`) %>%
+                # as.data.frame() %>%
+                sapply(round, roundto) %>%
+                as.data.frame() %>%
+                cbind(tidy(fit)$term) 
+        
+        colnames(fit_frame)[4] <- 'Term'
+        colnames(fit_frame)[2] <- '95% CI lower'
+        colnames(fit_frame)[3] <- '95% CI higher'
+        fit_frame %>% 
+                dplyr::select(term, estimate, `95% CI lower`, `95% CI higher`) %>%
+                kable()
+}
 
